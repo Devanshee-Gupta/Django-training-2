80a0a47b5c1d949029c867b2f04c4eff01e6abb1
Devanshee-Gupta <devansheegupta0@gmail.com>
2024-04-16 02:26:33 +0530

    Html file template added

--- ProjectDirectory/PollProject/home/views.py
+++ ProjectDirectory/PollProject/home/views.py
@@ -1,6 +1,23 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from home.serializers import ChoiceSerializer, QuestionSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from home.models import Choice, Question
 
 # Create your views here.
+@csrf_exempt
+@api_view(['GET'])
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    list_of_questions=[]
+    questionSerializer = QuestionSerializer(Question.objects.all(),many=True)
+
+    for i in questionSerializer.data:
+            choices = Choice.objects.filter(question__id=i['id'])
+            choicesSerializer=ChoiceSerializer(choices,many=True)
+            i['choices']=choicesSerializer.data
+            list_of_questions.append(i)
+
+    return render(request,"home.html",{'list_of_questions':list_of_questions})
+
+
